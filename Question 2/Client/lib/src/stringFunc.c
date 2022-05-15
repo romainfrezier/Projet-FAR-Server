27f5d9738ff846f7e72e861292229b250fc4b278
Romain FREZIER <romain.frezier@etu.umontpellier.fr>
2022-05-15 18:17:25 +0200

    little error with vscode

--- Question 2/Client/lib/src/stringFunc.c
+++ Question 2/Client/lib/src/stringFunc.c
@@ -2,41 +2,46 @@
 
 // Split command, with loop and argument n for generalization
 
-char** str_split(char *msg, int n)
+char **str_split(char *msg, int n)
 {
     int i = 0;
-    char* p = strtok(msg, " ");
-    char** array =(char**)malloc(n*sizeof(char*));
-    array[i] = (char*)malloc(strlen(p) + 1);
+    char *p = strtok(msg, " ");
+    char **array = (char **)malloc(n * sizeof(char *));
+    array[i] = (char *)malloc(strlen(p) + 1);
     strcpy(array[i], p);
-    i+= 1;
-    if (n > 2){
-        while (i < n - 1){
+    i += 1;
+    if (n > 2)
+    {
+        while (i < n - 1)
+        {
             p = strtok(NULL, " ");
-            array[i] = (char*)malloc(strlen(p) + 1);
-            strcpy(array[i], p); 
-            i+= 1;
+            array[i] = (char *)malloc(strlen(p) + 1);
+            strcpy(array[i], p);
+            i += 1;
         }
     }
     p = strtok(NULL, "\0");
-    array[i] = (char*)malloc(strlen(p) + 1);
+    array[i] = (char *)malloc(strlen(p) + 1);
     strcpy(array[i], p);
     return array;
 }
 
-int verifCommand(char* cmd, int nbSpace){
+int verifCommand(char *cmd, int nbSpace)
+{
     int spaceFound = 0;
-    for(int i = 0; i <= strlen(cmd); i++)
-  	{
-        if(cmd[i] == ' ')  
+    for (int i = 0; i <= strlen(cmd); i++)
+    {
+        if (cmd[i] == ' ')
         {
             spaceFound += 1;
         }
-	}
-    if (spaceFound >= nbSpace){
+    }
+    if (spaceFound >= nbSpace)
+    {
         return 1;
     }
-    else {
+    else
+    {
         return -1;
     }
 }
